0e3ef3b94e267c1daf134514ebb34cdf94266c23
codemaniac-sahil <sahilbisht19334@gmail.com>
2023-09-24 20:44:22 +0530

    sorted the blogs in order of created at date

--- controller/getAllblog.js
+++ controller/getAllblog.js
@@ -2,7 +2,7 @@ const Blog = require("../database/model/blog");
 
 const getAllBlog = async (req, res) => {
   const blogs = await Blog.find({})
-    .sort({ createdAt: -1 })
+    .sort({ createdAt: 1 })
     .populate(["likes.user", "comments.user"]);
   res.status(200).json(blogs);
 };
