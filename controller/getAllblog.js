7041dfb511ec3b4974547d50c7023ea22cef7f31
codemaniac-sahil <sahilbisht19334@gmail.com>
2023-09-24 20:52:07 +0530

    sorted

--- controller/getAllblog.js
+++ controller/getAllblog.js
@@ -1,9 +1,8 @@
 const Blog = require("../database/model/blog");
 
 const getAllBlog = async (req, res) => {
-  const blogs = await Blog.find({})
-    .populate(["likes.user", "comments.user"])
-    .sort({ createdAt: -1 });
+  const blogs = await Blog.find({}).populate(["likes.user", "comments.user"]);
+
   res.status(200).json(blogs);
 };
 module.exports = getAllBlog;
