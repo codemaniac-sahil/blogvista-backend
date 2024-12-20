678f087159ac3f5f0b88a4161a4457b71dae9611
Sahil Bisht <sahilbisht19334@gmail.com>
2024-12-20 21:47:15 +0530

    Fix log message for successful MongoDB connection

--- database/dbconnect.js
+++ database/dbconnect.js
@@ -5,7 +5,7 @@ env.config();
 const dbconnection = async () => {
   mongoose
     .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connection to CosmosDB successful"))
+    .then(() => console.log("Connection to MongosDB successful"))
     .catch((err) => console.error(err));
 };
 module.exports = dbconnection;
